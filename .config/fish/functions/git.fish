3efea5ffe023d35b3b944ad98bb1e2e22f26ac8e
qvshuo <qvshuo@foxmail.com>
2021-01-21 14:11:17 +0800

    rm wechat desktop

--- .config/fish/functions/git.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function git --wraps='proxychains git' --description 'alias git proxychains git'
-  proxychains git $argv;
-end
--- .config/fish/functions/yay.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function yay --wraps='proxychains yay' --description 'alias yay proxychains yay'
-  proxychains yay $argv;
-end
